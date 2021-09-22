4cfad7503a5942ed01a7786d982adea9e018140b
Curt Barnard <cbarnard@rapid7.com>
2021-09-22 13:40:02 -0700

    AdvancedSearchModal added

---
+++ src/components/AdvancedSearchModal.js
@@ -0,0 +1,165 @@
+import React, {
+  useState,
+  useEffect,
+} from 'react';
+import PropTypes from 'prop-types';
+
+import useAxios from "axios-hooks";
+import { API_URI } from '../config/constants';
+
+import { makeStyles } from '@material-ui/core/styles';
+import {
+  Grid,
+  TextField,
+  CircularProgress,
+} from '@material-ui/core';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    //maxWidth: 700,
+    padding: theme.spacing(2),
+  },
+}));
+
+const CPEFormAutocompleteItem = ({ field, setField, fieldName, queryParams }) => {
+  const [open, setOpen] = useState(false);
+  const [fieldText, setFieldText] = useState('');
+  const [{ data = [], loading }, executeRequest] = useAxios(
+    {
+      url: `${API_URI}/dictionary/typeahead`,
+    },
+    {
+      manual: true
+    });
+
+  useEffect(() => {
+    if (open) {
+      const myParams = { ...queryParams };
+      delete myParams[fieldName];
+      const delayRequest = setTimeout(() => {
+        executeRequest({
+          params: {
+            field: fieldName,
+            prefix: fieldText,
+            ...myParams,
+          }
+        }).catch(() => { });
+      }, 300);
+
+      return () => clearTimeout(delayRequest);
+    }
+  }, [fieldText, fieldName, executeRequest, open, queryParams]);
+
+  return (
+    <Autocomplete
+      freeSolo
+      id={`${fieldName}-select`}
+      open={open}
+      onOpen={() => {
+        setOpen(true);
+      }}
+      onClose={() => {
+        setOpen(false);
+      }}
+      onBlur={() => setField({ _id: fieldText })}
+      options={(!(data.find((item) => item._id === fieldText) || fieldText === '' )) ? [{ _id: fieldText }, ...data] : data}
+      value={field}
+      onChange={(event, newValue) => setField(newValue)}
+      inputValue={fieldText}
+      onInputChange={(event, newInputValue) => {
+        setFieldText(newInputValue);
+      }}
+      getOptionLabel={(option) => option._id || ""}
+      filterOptions={(x) => x}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={`${fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}`}
+          variant={"outlined"}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading ? <CircularProgress color="secondary" size={20} /> : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export const AdvancedSearchModal = () => {
+  const styles = useStyles();
+  const [queryParams, setQueryParams] = useState({});
+  const [vendor, setVendor] = useState('');
+  const [product, setProduct] = useState('');
+  const [version, setVersion] = useState('');
+
+  useEffect(() => {
+    setQueryParams((q) => {
+      let newParams = { ...q };
+      delete newParams.vendor;
+      return ({
+        ...newParams,
+        ...(vendor?._id && { vendor: vendor._id }),
+      });
+    });
+  }, [vendor, setQueryParams]);
+
+  useEffect(() => {
+    setQueryParams((q) => {
+      let newParams = { ...q };
+      delete newParams.product;
+      return ({
+        ...newParams,
+        ...(product?._id && { product: product._id }),
+      });
+    });
+  }, [product, setQueryParams]);
+
+  useEffect(() => {
+    setQueryParams((q) => {
+      let newParams = { ...q };
+      delete newParams.version;
+      return ({
+        ...newParams,
+        ...(version?._id && { version: version._id }),
+      });
+    });
+  }, [version, setQueryParams]);
+
+  return (
+    <div className={styles.root}>
+      <Grid container spacing={3}>
+        <Grid item sm={4}>
+          <Autocomplete
+            id={"combo-box-demo"}
+            options={['a', 'o', 'h']}
+            renderInput={(params) => <TextField {...params} label={"Part"} variant={"outlined"} />}
+          />
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <CPEFormAutocompleteItem field={vendor} setField={setVendor} fieldName={'vendor'} queryParams={queryParams} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <CPEFormAutocompleteItem field={product} setField={setProduct} fieldName={'product'} queryParams={queryParams} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <CPEFormAutocompleteItem field={version} setField={setVersion} fieldName={'version'} queryParams={queryParams} />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+AdvancedSearchModal.propTypes = {
+};
+
+AdvancedSearchModal.defaultProps = {
+};
+
+export default AdvancedSearchModal;
---
+++ src/stories/components/AdvancedSearchModal.stories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AdvancedSearchModal from '../../components/AdvancedSearchModal';
+
+export default {
+  title: 'Components/AdvancedSearchModal',
+  component: AdvancedSearchModal,
+};
+
+const Template = (args) => <AdvancedSearchModal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+
