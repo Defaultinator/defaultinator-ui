61100a49edf37ef42612ad23c7d0ce9cfe5e2f70
Curt Barnard <cbarnard@rapid7.com>
2022-01-05 15:11:43 -0800

    updated ports

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,13 +1,10 @@
-const DEFAULT_PORT = process.env.PORT || 3001;
-
 const API_BASE_URI = process.env.API_BASE_URI || "http://localhost";
-const API_PORT = process.env.API_PORT || 3000;
+const API_PORT = process.env.API_PORT || 3001;
 const API_URI = `${API_BASE_URI}:${API_PORT}`;
 
 const REQUEST_ACCOUNT_EMAIL = 'cbarnard@rapid7.com';
 
 module.exports = {
-  DEFAULT_PORT,
   API_URI,
   REQUEST_ACCOUNT_EMAIL,
 };
