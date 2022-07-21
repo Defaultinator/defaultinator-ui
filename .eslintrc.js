8b013483b5049a57abebd13b5b962ee0515ae4e3
Curt Barnard <curt_barnard@rapid7.com>
2022-07-21 15:16:26 -0400

    lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,5 +28,10 @@ module.exports = {
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
     'react/no-array-index-key': 'off',
+    'react/prop-types': 'off', // TODO: Add all prop types in a refactor
+    'react/require-default-props': 'off', // TODO: Same as above
+    'react/default-props-match-prop-types': 'off', // TODO: Same as above
+    'react/forbid-prop-types': 'off', // TODO: Same as above
+    'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.js', '**/*.stories.js', 'setupTests.js'] }],
   },
 };
