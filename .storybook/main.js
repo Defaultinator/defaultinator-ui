57b07726fee063fc74cf565018a2b60e46f86a5a
Curt Barnard <curt_barnard@rapid7.com>
2022-07-21 15:51:44 -0400

    Disable linting barrier for chromatic/storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,8 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
+    "@storybook/addon-interactions"
+    // TODO: Enable thisd after linting "@storybook/preset-create-react-app"
   ],
   "framework": "@storybook/react",
   "core": {
